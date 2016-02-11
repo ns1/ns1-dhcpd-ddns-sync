2bfd1df27ed46b26c26ac30128793624363864c4
Nate Lindstrom <nlindstrom@ns1.com>
2016-02-11 12:46:04 -0800

    Updated license

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # ns1-dhcpd-ddns-sync
 Tool to synchronize NS1 DNS record management with an ISC DHCPD server
+
+## Installation
+Create an API key in the NS1 portal if you have not already done so by going to **Account** -> **Settings & Users** -> **Manage API Keys** -> **Add a New Key**.
+
+Create a zone to contain your forward `A` records (`example.com`) and a zone to contain your reverse `PTR` records (`in-addr.arpa`) in the NS1 portal if you have not already done so by going to **Zones** -> **Add a New Zone**.
+
+Copy the `ns1-dhcpd-ddns-sync.pl` script to a suitable location on your DHCP server, such as `/usr/local/bin`.
+
+Add it to root's crontab so it executes frequently, ideally every minute:
+
+`* * * * * /usr/local/bin/ns1-dhcpd-ddns-sync.pl --api-key=<key> --forward-zone=<zone> --reverse-zone=<zone>`
--- ns1-dhcpd-ddns-sync.pl
+++ ns1-dhcpd-ddns-sync.pl
@@ -1,14 +1,25 @@
 #!/usr/bin/env perl
-#####################################################################
-#                                                                   #
-# Synchronizes active  DHCP  leases with the  specified forward and #
-# reverse zones, including removing DNS entries for expired leases. #
-#                                                                   #
-# Version 1.0 by Nate Lindstrom <nlindstrom@ns1.com>                #
-#                                                                   #
-# Copyright (c) 2016 NS1, all rights reserved.                      #
-#                                                                   #
-#####################################################################
+#########################################################################
+#                                                                       #
+# Synchronizes active  DHCP  leases with the  specified forward and     #
+# reverse zones, including removing DNS entries for expired leases.     #
+#                                                                       #
+# Copyright (c) NS1, version 1.0 by Nate Lindstrom <nlindstrom@ns1.com> #
+#                                                                       #
+# This program is free software: you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation, either version 3 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. #
+#                                                                       #
+#########################################################################
 
 use strict;
 use warnings;
